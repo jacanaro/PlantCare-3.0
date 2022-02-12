c48a7a04eb3856526f2e87586a4894ac616ec2cb
Dominik <dominik.rezmer@gmail.com>
2022-02-12 14:44:36 +0100

    XML Dokumentation

--- PlantCare_Variante/Assets/Scripts/Script_BtnAnim.cs
+++ PlantCare_Variante/Assets/Scripts/Script_BtnAnim.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script_BtnAnim: MonoBehaviour
+public class Script_BtnAnim : MonoBehaviour
 {
     public GameObject Panel;
 
+    /// <summary>
+    /// Fährt das Burger-Menü mit einer Animation aus und ein.
+    /// </summary>
     public void OpenPanel()
     {
-        if(Panel != null)
+        if (Panel != null)
         {
             Animator animator = Panel.GetComponent<Animator>();
-            if(animator != null)
+            if (animator != null)
             {
                 bool isOpen = animator.GetBool("open");
+
                 animator.SetBool("open", !isOpen);
             }
         }
     }
-
 }
